1047a68a00e0d36447a68cb75a80fc311dc5b61f
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-24 06:31:17 +0000

    Update to correct pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,8 +4,9 @@ that returns a list of lists of integers representing
 the Pascal’s triangle of n:
 
 Returns an empty list if n <= 0
-You can assume n will be always an integer 
+You can assume n will be always an integer
 you are not allowed to import any module"""
+
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing
