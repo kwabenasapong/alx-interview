c871d547d88b88bbe012ba1dea14e2ee1ebd2bd5
Kwabena Sapong <kwabenasapong@gmail.com>
2023-03-24 06:27:37 +0000

    Update 3 to solution for question

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
+""" Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal’s triangle of n:
 
 Returns an empty list if n <= 0
 You can assume n will be always an integer """
@@ -7,7 +9,8 @@ import math
 
 
 def pascal_triangle(n):
-    """ Returns a list of lists of integers representing the Pascal’s triangle of n """
+    """ Returns a list of lists of integers
+    representing the Pascal’s triangle of n """
     if n <= 0:
         return []
     else:
@@ -22,4 +25,3 @@ def pascal_triangle(n):
 
 if __name__ == "__main__":
     pascal_triangle(n)
-    
